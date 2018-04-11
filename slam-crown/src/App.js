c63bf2cdd33c0aa9ace4a4678879fd31c3634404
Zack Toth <zacharyrtoth@gmail.com>
2018-04-10 21:06:18 -0700

    added slidingscale files

--- slam-crown/src/App.js
+++ slam-crown/src/App.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Home from './home';
+import Board from './board';
+
+
+export default function App(props) {
+    return (
+        <Router>
+            <div className="app">
+                <header>
+                    <h1><Link to="/">Slam Crown</Link></h1>
+                </header>
+                <main>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/board/:boardId" component={Board} />
+                </main>
+            </div>
+        </Router>
+    );
+}
+
 class App extends Component {
   render() {
     return (
