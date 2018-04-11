15cb81746c43fc3747f6eb5d7f3312ff695b4de8
Zack Toth <zacharyrtoth@gmail.com>
2018-04-10 23:03:17 -0700

    added react-router dep

--- slam-crown/src/App.js
+++ slam-crown/src/App.js
@@ -3,40 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import Home from './home';
-import Board from './board';
 
+import LandingPage from './landingpage.js';
 
 export default function App(props) {
     return (
         <Router>
             <div className="app">
                 <header>
-                    <h1><Link to="/">Slam Crown</Link></h1>
+                    <h1><Link to="/">Welcome to Slam Crown</Link></h1>
                 </header>
                 <main>
-                    <Route exact path="/" component={Home} />
+                    <Route exact path="/" component={LandingPage} />
                     <Route exact path="/board/:boardId" component={Board} />
                 </main>
             </div>
         </Router>
     );
 }
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Slam Crown</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
