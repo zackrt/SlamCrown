6fc057a99a1e4562b6a4bd2ec6d5961d0af5ded6
Zack Toth <zacharyrtoth@gmail.com>
2018-04-11 19:23:22 -0700

    added jsx to userpage.js

--- slam-crown/src/components/login.js
+++ slam-crown/src/components/login.js
@@ -3,9 +3,19 @@ import React, { Component } from 'react'
 export class Login extends Component {
   render() {
     return (
-      <div>
-        Login page
-      </div>
+        <div>
+        <h1>
+        Login to Slam Crown <i class="fi-crown"></i>
+        </h1>
+        <form>
+    <fieldset>
+            <legend>Login:</legend>
+            Email: <input type="text" placeholder="demo@slamcrown.com" id="email-address"required /><br/>
+            Password: <input type="text" placeholder="password" id="user-password" required /><br/>  
+    </fieldset>
+           <input type="submit" value="Login!" />
+          </form>
+          </div>
     )
   }
 }
