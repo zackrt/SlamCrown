9fa74d91dfff75c5516513a48a52dfd4b10104be
Zack Toth <zacharyrtoth@gmail.com>
2018-04-13 21:19:06 -0700

    added descriptions

--- slam-crown/src/components/landingpage.js
+++ slam-crown/src/components/landingpage.js
@@ -7,7 +7,22 @@ export class LandingPage extends Component {
       <div>
         Welcome to Slam Crown!!
         <a href="/sign-up">Sign Up now!</a>
-        This is a simple concussion & TBI report app 
+        <h1>This is a simple and easy concussion & Traumatic Brain Injury reporting app</h1>
+          <h2>What does Slam Crown do?</h2>
+            <ul className="instructions">
+              <li classname="instruct-li">
+                If you have ever had a concussion or you are recovering from mild traumatic brain injury, create a free account!
+              </li>
+              <li classname="instruct-li">
+                You can track your daily symptom recovery
+              </li>
+              <li classname="instruct-li">
+                Note the occurences of headaches, fevers and fatigue, etc.
+              </li>
+              <li classname="instruct-li">
+                * Slam Crown is not method or replacement for medical treatment!
+              </li>       
+            </ul>
       </div>
     )
   }
